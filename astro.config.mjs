ecf7d643bae6dc9ac9ec5a3920f3c62a1f6812d1
Ryan Ellis <ryan.ellis@thehutgroup.com>
2023-06-28 09:40:12 +0100

    Repository setup from template

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+
+export default defineConfig({
+  adapter: cloudflare(),
+  output: 'server',
+  build: {
+    assets: 'ssr-assets'
+  },
+});
